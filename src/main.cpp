206f710690b79bb865399bf9b3e23eff3e640e55
MrAosix <emeryk.belisle@hotmail.com>
2025-09-09 17:15:39 -0400

    exercice1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <Arduino.h>
+#include <MicroOscSlip.h>
+
+#define MA_BROCHE_ANGLE 32
+#define MA_BROCHE_BOUTON 39
+
+MicroOscSlip<128> monOsc(&Serial);
 
 void setup() {
+  Serial.begin(115200);
+  pinMode( MA_BROCHE_BOUTON , INPUT );
+
 }
 
 void loop() {
+  int maLectureAnalogique = analogRead(MA_BROCHE_ANGLE);
+  int maLectureBouton = digitalRead( MA_BROCHE_BOUTON );
+  //Serial.println(maLectureBouton);
+  monOsc.sendInt( "/bouton" , maLectureBouton);
+  monOsc.sendInt( "/analog" , maLectureAnalogique);
+  delay(100);
+
 }
